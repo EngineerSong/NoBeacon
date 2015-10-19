6e2e79526c6f87ed8413c24121fb8681a91bbd9e
barara <barara@bararadeimac.airdream>
2015-10-19 15:58:26 +0800

    Initial Commit

---
+++ NoBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoBeacon
+//
+//  Created by barara on 15/10/19.
+//  Copyright © 2015年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoBeacon
+//
+//  Created by barara on 15/10/19.
+//  Copyright © 2015年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
